0bf6c98a2082ab406d193f22876c21c864c6b3d5
Chen Stanilovsky <chen@chens-air.myfiosgateway.com>
2021-04-21 15:36:20 -0400

    Worked on raos.py

--- baseline_4/raos_algo/raos.py
+++ baseline_4/raos_algo/raos.py
@@ -1,9 +1,17 @@
 from player import *
+import enum
+
+class KB(enum.Enum):
+   Color = 0
+   Positions = 1
 
 class RaosAlgorithm(Player):
     def __init__(self):
         self.player_name = "RaosAlgorithm"
-        self.inferences = []
+
+        # inferences[i][0] is a color, inferences[i][1] is a list of possible positions for that color
+        self.inferences = [] 
+
         self.being_considered = None
         self.being_fixed = None
     
@@ -18,31 +26,76 @@ class RaosAlgorithm(Player):
                                            code for the previous guess and the second element is the number of pegs that are 
                                            the right color, but in the wrong location for the previous guess.
         """
+        return
+
+    def tied(self, pos):
+        """
+        Returns if the given position in the guess is tied to a specific color
+
+        Parameters
+        ----------
+        pos - The given position in the trial/guess
+        """
+        for i in range(len(self.inferences)):
+            # Look at each color in inferences, if the possible positions is just 1 position (tied), see if it matches the parameter pos
+            # If there is a match, that position is tied to a color
+            if len(self.inferences[i][KB.Positions]) == 1 and self.inferences[i][KB.Positions][0] == pos:
+                return True
+        return False
+
+    def itscolor(self, tied_pos):
+        """
+        Returns the color that a tied position is tied to
+
+        Parameters
+        ----------
+        tied_pos - the position in the trial that is tied to a color
+        """
+        for i in range(len(self.inferences)):
+            # See if this entry from inferences only has one position in possible positions
+            # If position matches the tied_pos, return the color that the position is tied to
+            if len(self.inferences[i][KB.Positions]) == 1 and self.inferences[i][KB.Positions][0] == tied_pos:
+                return self.inferences[i][KB.Color]
+            
+
+    def nextpos(self, color):
+         """
+        Returns the next possible position for the color 
+
+        Parameters
+        ----------
+        tied_pos - the position in the trial that is tied to a color
+        """
+
+    def Getnext(self,trial):
+        """
+        Returns next guess
+
+        Parameters
+        ----------
+        trial - Previous guess
+        """
+        N = len(trial)
+        new_trial = []
+        for i in range (N):
+            if self.tied(i):
+                new_trial.append(self.itscolor(i))
+            elif i == self.nextpos(self.being_fixed):
+                new_trial.append(self.being_fixed)
+            elif len(self.inferences) == N:
+                new_trial.append(secondunfixed(self.inferences))
+            else:
+                new_trial.append(self.being_considered)
+        return new_trial
 
 
-
-
-def Getnext(trial, inferences, being_considered, being_fixed):
-    """
-    Returns next guess
-
-    Parameters
-    ----------
-    trial - Previous guess
-    inferences - Knowledge base
-    being_considered - Color that is being considered to see if it is part of the code
-    being_fixed - Color that has been determined to be part of the code and now trying to find the right position
-    """
-    
-
-
-def Try(trial):
-    print(f"My Next Trial: {trial}")
-    print("Enter Bulls >> ", end="")
-    bulls = input()
-    print("Enter Cows >> ", end="")
-    cows = input()
-    return bulls, cows
+    def Try(self, trial):
+        print(f"My Next Trial: {trial}")
+        print("Enter Bulls >> ", end="")
+        bulls = input()
+        print("Enter Cows >> ", end="")
+        cows = input()
+        return bulls, cows
 
 def MasterMind(N,M):
     """
