89c05cbecd822cc1b6ad985b922c1cba083ede6a
Rinchen Lama <rinchenlama0075@gmail.com>
2021-05-02 15:32:32 -0400

    only comments on raos.py

--- fbi_player/raos_algo/raos.py
+++ fbi_player/raos_algo/raos.py
@@ -1,20 +1,26 @@
 from player import *
 import enum
 
+
 class KB(enum.Enum):
-   Color = 0
-   Positions = 1
+    Color = 0
+    Positions = 1
+
 
 class RaosAlgorithm(Player):
     def __init__(self):
         self.player_name = "RaosAlgorithm"
 
         # inferences[i][0] is a color, inferences[i][1] is a list of possible positions for that color
-        self.inferences = [] 
+        self.inferences = []
 
-        self.being_considered = None
-        self.being_fixed = None
-    
+        # increase once every turn to try a different color
+        self.being_considered = 0
+
+        # initial value to correspond to first color determined to be in the code
+        # increase once a position is fixed for a color
+        self.being_fixed = -1
+
     def make_guess(self, board_length, colors, scsa, last_response):
         """Makes a guess of the secret code for Mastermind
 
@@ -56,18 +62,17 @@ class RaosAlgorithm(Player):
             # If position matches the tied_pos, return the color that the position is tied to
             if len(self.inferences[i][KB.Positions]) == 1 and self.inferences[i][KB.Positions][0] == tied_pos:
                 return self.inferences[i][KB.Color]
-            
 
     def nextpos(self, color):
-         """
-        Returns the next possible position for the color 
-
-        Parameters
-        ----------
-        tied_pos - the position in the trial that is tied to a color
         """
+       Returns the next possible position for the color 
 
-    def Getnext(self,trial):
+       Parameters
+       ----------
+       tied_pos - the position in the trial that is tied to a color
+       """
+
+    def Getnext(self, trial):
         """
         Returns next guess
 
@@ -77,7 +82,7 @@ class RaosAlgorithm(Player):
         """
         N = len(trial)
         new_trial = []
-        for i in range (N):
+        for i in range(N):
             if self.tied(i):
                 new_trial.append(self.itscolor(i))
             elif i == self.nextpos(self.being_fixed):
@@ -88,7 +93,6 @@ class RaosAlgorithm(Player):
                 new_trial.append(self.being_considered)
         return new_trial
 
-
     def Try(self, trial):
         print(f"My Next Trial: {trial}")
         print("Enter Bulls >> ", end="")
@@ -97,36 +101,40 @@ class RaosAlgorithm(Player):
         cows = input()
         return bulls, cows
 
-def MasterMind(N,M):
-    """
-    MasterMind main method
 
-    Parameters
-    ----------
-    N - Number of positions/board size
-    M - Number of colors
-        Colors are represented as (1, 2, ..., M)
-    """
-        
-    trial = []
-    inferences = []
-    being_considered = "A"
-    being_fixed = None
-    # setup() TODO, inialize first trial, reset knowledge base
-    bulls = 0
-    cows = 0
-    bulls, cows = Try(trial)
-    gameover = False
-    while bulls < N and not gameover:
-        #Update(inferences) (updates inferences based on number of bulls, cows, and previous trial)
-        #Getnext(trial, inferences, Being-Considered, Being-Fixed) (updates trial based on new inferences)
-        #if Numfix(inferences) == N: (goal test)
-            #gameover = True
-        # else:
-            #  Try(trial)    
-    print(trial)
-def main():
-    help(MasterMind)
+# def MasterMind(N, M):
+#     """
+#     MasterMind main method
 
-if __name__ == "__main__":
-    main()
+#     Parameters
+#     ----------
+#     N - Number of positions/board size
+#     M - Number of colors
+#         Colors are represented as (1, 2, ..., M)
+#     """
+
+#     trial = []
+#     inferences = []
+#     being_considered = "A"
+#     being_fixed = None
+#     # setup() TODO, inialize first trial, reset knowledge base
+#     bulls = 0
+#     cows = 0
+#     bulls, cows = Try(trial)
+#     gameover = False
+#     while bulls < N and not gameover:
+#         # Update(inferences) (updates inferences based on number of bulls, cows, and previous trial)
+#         # Getnext(trial, inferences, Being-Considered, Being-Fixed) (updates trial based on new inferences)
+#         # if Numfix(inferences) == N: (goal test)
+#             #gameover = True
+#         # else:
+#             #  Try(trial)
+#     print(trial)
+
+
+# def main():
+#     help(MasterMind)
+
+
+# if __name__ == "__main__":
+#     main()
